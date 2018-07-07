70b26a272c4f8959c1a3cfcdd590e5df96a9cb52
Venkatesh Jayaraman <venkatesh.j@bankbazaar.com>
2018-07-07 17:10:49 +0530

    Added the capability of supporting EMAIL also

--- lead-dropoff-analysis.sh
+++ lead-dropoff-analysis.sh
@@ -1,10 +1,12 @@
-while getopts ":d:s:o:" opt; do
+while getopts ":d:s:o:f" opt; do
     case $opt in
         s) src_dir="$OPTARG"
         ;;
         d) for_date="$OPTARG"
         ;;
         o) out_dir="$OPTARG"
+        ;;
+        f) force_init=true
         ;;
     esac
 done
@@ -97,51 +99,94 @@ declare user_agent_family_awk_fn='function compute_ua_family(user_agent) {
 }
 '
 
+center_print() {
+    termwidth=100; #"$(tput cols)"
+    padding="$(printf '%0.1s' \#{1..500})"
+    printf '%*.*s %s %*.*s\n' 0 "$(((termwidth-2-${#1})/2))" "$padding" "$1" 0 "$(((termwidth-1-${#1})/2))" "$padding"
+}
+
 function print_progress() {
     completed_ops_count="$1"
     total_ops_count="$2"
     msg="$3"
-    perc_completed=$((completed_ops_count*100/total_ops_count))
-    printf "$msg: Completed: % -101s(%s%%)\r" $(head -c $perc_completed < /dev/zero | tr '\0' '#') $perc_completed;
+    skipped=$4
+
+    if [ -e $skipped ]; then
+        skipped=false
+    fi
+
+    color_none='\033[0m'
+    color_cyan='\033[1;36m';
+    color_yellow='\033[1;33m'
+    color_green='\033[1;32m'
+
+    if [ $skipped == true ]; then
+        printf "%-65s: ${color_cyan}SKIPPED${color_none}\n" "$msg";
+    else
+        perc_completed=$((completed_ops_count*100/total_ops_count))
+        if [ $perc_completed == 100 ]; then
+            printf "%-65s: Completed: ${color_green}% -51s(%s%%)${color_none}\r" "$msg" $(head -c $((perc_completed/2)) < /dev/zero | tr '\0' '#') $perc_completed;
+        else
+            printf "%-65s: Completed: ${color_yellow}% -51s(%s%%)${color_none}\r" "$msg" $(head -c $((perc_completed/2)) < /dev/zero | tr '\0' '#') $perc_completed;
+        fi
+    fi
 }
 
 function initialize() {
-    echo "Initializing the runtime"
+    if [ "$force_init" == true ]; then
+        echo "Force Reset is set. Deleting pre-generated artifacts"
+        rm -rf "${out_dir}/${for_date}"
+    fi
+
+    local op_name="Creating $in_dir"
     if [ ! -d "$in_dir" ]; then
-        echo "    Creating the IN directory $in_dir"
+        print_progress 1 1 "$op_name"
+        printf "\n"
         mkdir -p "$in_dir"
 
-        progress_msg="        Copying relevant files"
+        local op_name="    Copying relevant files"
         number_of_containers=$(ls $src_dir | grep B.* | wc -l)
         total_ops_count=$((${#files_to_be_parsed[@]}*2*number_of_containers))
         i=0
-        print_progress $i $total_ops_count "$progress_msg" 
+        print_progress $i $total_ops_count "$op_name" 
         for cntr_dir in $src_dir/B*; do
             container_name=$(basename ${cntr_dir})
             for fn in ${files_to_be_parsed[@]}; do
                 if [ -f ${src_dir}/${container_name}/DAS/tomcat/logs/${fn}.${for_date}.gz ]; then
                     cp ${src_dir}/${container_name}/DAS/tomcat/logs/${fn}.${for_date}.gz ${in_dir}/${fn}.${for_date}.${container_name}.gz
-                    print_progress $((++i)) $total_ops_count "$progress_msg" 
+                    print_progress $((++i)) $total_ops_count "$op_name" 
                     gunzip ${in_dir}/${fn}.${for_date}.${container_name}.gz
-                    print_progress $((++i)) $total_ops_count "$progress_msg" 
+                    print_progress $((++i)) $total_ops_count "$op_name" 
                 else
                     i=$((i+2))
-                    print_progress $i $total_ops_count "$progress_msg" 
+                    print_progress $i $total_ops_count "$op_name" 
                 fi
             done
         done
         printf "\n"
+    else
+        print_progress 1 1 "$op_name" true
     fi
 
+    local op_name="Creating $out_dir"
     if [ ! -d "$out_dir" ]; then
-        echo "    Creating the OUT directory $out_dir"
+        print_progress 1 1 "$op_name"
         mkdir -p "$out_dir"
+    else
+        print_progress 1 1 "$op_name" true
     fi
 }
 
 function parse_session_ids_which_got_CC_SL() {
-    echo "Searching for the SL SMS access patterns in clickstream.log files..."
-    grep "credit-card-smart-landing.*smsCampaign=true" $in_dir/clickstream.log.* \
+    local campaign_type=$1
+    local op_name="Computing SessionIDs which opened CC-SL '$campaign_type' from ClickStream";
+    if [ -f $out_dir/"$campaign_type"_session_id_to_ua.txt ]; then
+        print_progress 1 1 "$op_name" true 
+        return
+    fi
+
+    echo "$op_name"
+    grep "credit-card-smart-landing.*${campaign_type}Campaign=true" $in_dir/clickstream.log.* \
         | awk -F : "$user_agent_family_awk_fn"'{
             split($0, tokens, ""); 
             ip_address = tokens[2];
@@ -152,22 +197,28 @@ function parse_session_ids_which_got_CC_SL() {
         }' \
         > $out_dir/tmp_clickstream_sl_alone.txt
 
-    echo "Computing the SessionIDs which opened CC SL SMSCampaign..."
-    sort $out_dir/tmp_clickstream_sl_alone.txt | uniq > "$out_dir"/session_id_to_ua_including_blacklisted.txt
-    grep -v "FirefoxBlacklisted" $out_dir/session_id_to_ua_including_blacklisted.txt > "$out_dir"/session_id_to_ua.txt
+    sort $out_dir/tmp_clickstream_sl_alone.txt | uniq > "$out_dir"/"$campaign_type"_session_id_to_ua_including_blacklisted.txt
+    grep -v "FirefoxBlacklisted" $out_dir/"$campaign_type"_session_id_to_ua_including_blacklisted.txt > "$out_dir"/"$campaign_type"_session_id_to_ua.txt
     rm $out_dir/tmp_clickstream_sl_alone.txt
 }
 
 function categorize_sessions_based_on_treatments() {
-    awk '{print $1}' "$out_dir"/session_id_to_ua.txt > "$out_dir"/tmp_session_ids_only.txt
+    local campaign_type=$1
+    local op_name="Categorizing '$campaign_type' Sessions by Treatments"
+
+    if [ -f "$out_dir"/"$campaign_type"_session_ids-t1.txt ]; then
+        print_progress 1 1 "$op_name" true
+        return
+    fi
+
+    awk '{print $1}' "$out_dir"/"$campaign_type"_session_id_to_ua.txt > "$out_dir"/tmp_session_ids_only.txt
 
     number_of_chunks=$(split --verbose -l 3000 -d --additional-suffix .txt $out_dir/tmp_session_ids_only.txt tmp_session_ids-chunk- | wc -l)
     total_ops_count=$((number_of_chunks+1+${#treatments[@]}))
     mv tmp_session_ids-chunk-* $out_dir/.
 
     i=0
-    progress_msg="Categorizing the sessions based on assigned treatments"
-    print_progress $i $total_ops_count "$progress_msg" 
+    print_progress $i $total_ops_count "$op_name" 
     for chunk in "$out_dir"/tmp_session_ids-chunk-*.txt; do
         ./agrep -f $chunk $in_dir/LoanDukaan.log.* \
             | grep "The CAMPAIGN_KEY session attribute modified to .*${experiment_id}t*" \
@@ -176,14 +227,14 @@ function categorize_sessions_based_on_treatments() {
             | sort | uniq | awk '{print $1 "\t" $2}' \
             >> "$out_dir"/tmp_session_ids_categorization_1.txt
 
-        print_progress $((++i)) $total_ops_count "$progress_msg" 
+        print_progress $((++i)) $total_ops_count "$op_name" 
     done
 
-    join $out_dir/session_id_to_ua.txt $out_dir/tmp_session_ids_categorization_1.txt > $out_dir/tmp_session_ids_categorization_2.txt
-    print_progress $((++i)) $total_ops_count "$progress_msg" 
+    join $out_dir/"$campaign_type"_session_id_to_ua.txt $out_dir/tmp_session_ids_categorization_1.txt > $out_dir/tmp_session_ids_categorization_2.txt
+    print_progress $((++i)) $total_ops_count "$op_name" 
     for treatment in "${treatments[@]}"; do
-        grep ".*${treatment}$" $out_dir/tmp_session_ids_categorization_2.txt | sort > "$out_dir"/session_ids-"$treatment".txt
-        print_progress $((++i)) $total_ops_count "$progress_msg" 
+        grep ".*${treatment}$" $out_dir/tmp_session_ids_categorization_2.txt | sort > "$out_dir"/"$campaign_type"_session_ids-"$treatment".txt
+        print_progress $((++i)) $total_ops_count "$op_name" 
     done
 
     printf "\n"
@@ -191,14 +242,19 @@ function categorize_sessions_based_on_treatments() {
 }
 
 function compute_session_activity() {
-    rm -f "$out_dir"/session_activity-*.txt
+    local campaign_type=$1
+    local op_name="Computing the Session Activity"
+
+    if [ -f "$out_dir"/"$campaign_type"_session_activity-t1.txt ]; then
+        print_progress 1 1 "$op_name" true
+        return
+    fi
 
     i=0
     total_ops_count=$((${#treatments[@]}*3))
-    progress_msg="Computing the Session Activity"
-    print_progress $i $total_ops_count "$progress_msg" 
+    print_progress $i $total_ops_count "$op_name" 
     for treatment in "${treatments[@]}"; do
-        awk '{print $1}' "$out_dir"/session_ids-"$treatment".txt > "$out_dir"/tmp_session_ids_only-"$treatment".txt
+        awk '{print $1}' "$out_dir"/"$campaign_type"_session_ids-"$treatment".txt > "$out_dir"/tmp_session_ids_only-"$treatment".txt
 
         ./agrep -f "$out_dir"/tmp_session_ids_only-"$treatment".txt $in_dir/clickstream.log.* \
             | awk -F : "$user_agent_family_awk_fn"'{
@@ -212,19 +268,19 @@ function compute_session_activity() {
                 gsub("&origin_path.*$", "", request_url);
                 print session_id "\t" ip_address "\t" user_agent_family "\t" "\""user_agent"\"" "\t" request_url "\t" originating_url
             }' \
-            > "$out_dir"/clickstream-"$treatment".txt
-        print_progress $((++i)) $total_ops_count "$progress_msg" 
+            > "$out_dir"/"$campaign_type"_clickstream-"$treatment".txt
+        print_progress $((++i)) $total_ops_count "$op_name" 
 
-        ./agrep -f "$out_dir"/tmp_session_ids_only-"$treatment".txt $in_dir/localhost_access.log.* > "$out_dir"/localhost_access-"$treatment".txt
-        cat -n "$out_dir"/localhost_access-"$treatment".txt \
+        ./agrep -f "$out_dir"/tmp_session_ids_only-"$treatment".txt $in_dir/localhost_access.log.* > "$out_dir"/"$campaign_type"_localhost_access-"$treatment".txt
+        cat -n "$out_dir"/"$campaign_type"_localhost_access-"$treatment".txt \
             | awk 'BEGIN{ OFS="\t" }{print $13, $1, $6"]", $8 " " $9 " " $10, $11}' \
             | sort -k1,1 -k2,2n \
             | awk 'BEGIN{ FS=OFS="\t" }{print $1, $3, $4, $5}' \
             > "$out_dir"/tmp_localhost_access-"$treatment".txt
-        join -1 1 -2 1 -t $'\t' $out_dir/session_id_to_ua.txt "$out_dir"/tmp_localhost_access-"$treatment".txt > "$out_dir"/localhost_access-"$treatment".txt
-        print_progress $((++i)) $total_ops_count "$progress_msg" 
+        join -1 1 -2 1 -t $'\t' $out_dir/"$campaign_type"_session_id_to_ua.txt "$out_dir"/tmp_localhost_access-"$treatment".txt > "$out_dir"/"$campaign_type"_localhost_access-"$treatment".txt
+        print_progress $((++i)) $total_ops_count "$op_name" 
 
-        grep -Ev "$ignore_url_patterns" "$out_dir"/localhost_access-"$treatment".txt | sed "$replace_url_patterns" \
+        grep -Ev "$ignore_url_patterns" "$out_dir"/"$campaign_type"_localhost_access-"$treatment".txt | sed "$replace_url_patterns" \
             | awk 'BEGIN{ FS=OFS="\t" }{
                 if (NR == 0) {
                     last_session_id = ""
@@ -241,74 +297,82 @@ function compute_session_activity() {
                 last_session_id = curr_session_id;
                 last_session_info = $1 "\t" $2 "\t" $3 "\t" $4 "\t"
             } END {print last_session_info, aggr_activity}' \
-            >> "$out_dir"/session_activity-"$treatment".txt
-        print_progress $((++i)) $total_ops_count "$progress_msg"
+            >> "$out_dir"/"$campaign_type"_session_activity-"$treatment".txt
+        print_progress $((++i)) $total_ops_count "$op_name"
 
         rm "$out_dir"/tmp_localhost_access-"$treatment".txt "$out_dir"/tmp_session_ids_only-"$treatment".txt
     done
     printf "\n"
 }
 
+function analyze_requests_wo_ajaxcontent() {
+    local campaign_type=$1
+    local op_name="Analyzing UserAgents"
+
+    if [ -f "$out_dir"/"$campaign_type"_ua_analysis_proper_wo_ajax_content.txt ]; then
+        print_progress 1 1 "$op_name" true
+        return
+    fi
+    echo $op_name
+
+    grep -P "okhttp" "$out_dir"/"$campaign_type"_session_activity-t*.txt \
+        | awk '{print $2}' \
+        | grep -f - "$out_dir"/"$campaign_type"_localhost_access-t*.txt \
+        | grep -v okhttp \
+        | awk 'BEGIN {FS="\t"}{print $4}' \
+        | sed -r 's/.*\(.*Android [.0-9]*; //g; s/\).*//g; s/en-[a-zA-Z]{2}; //g; s/; wv//g' \
+        | sort | uniq \
+        > "$out_dir"/"$campaign_type"_ua_analysis_emitting_devices.txt
+    echo "    Devices which requested with UserAgent okhttp has been written to " "$out_dir"/"$campaign_type"_ua_analysis_emitting_devices.txt
+
+    grep "SL_LANDING,$" "$out_dir"/"$campaign_type"_session_activity-*.txt \
+        | grep -v okhttp \
+        | awk 'BEGIN{OFS=FS="\t"}{print $3,$4}' \
+        | sort  | uniq \
+        > "$out_dir"/"$campaign_type"_ua_analysis_proper_wo_ajax_content.txt
+    echo "    Proper UserAgents (not okhttp) for which we didnt see ajax-content call are exported to " "$out_dir"/"$campaign_type"_ua_analysis_proper_wo_ajax_content.html
+}
+
 function print_summary() {
-    echo ""
-    echo "*************************************************************************************************************"
-    echo ""
-    echo "Summary of SL accesses"
-    echo "Number of Unique Sessions = " `cat ${out_dir}/session_ids-t1.txt ${out_dir}/session_ids-t2.txt ${out_dir}/session_ids-t3.txt | wc -l`
-    echo ""
+    local campaign_type=$1
+    echo "Number of Unique Sessions = " `cat ${out_dir}/"$campaign_type"_session_ids-t1.txt ${out_dir}/"$campaign_type"_session_ids-t2.txt ${out_dir}/"$campaign_type"_session_ids-t3.txt | wc -l`
     printf "%10s %15s %15s %20s %5s %20s %20s\n" "Treatment" "Sessions#" "SLAccess#" "SlideshowRendered#" "%" "DirectToSearch#" "WrongZeroOffersInT3"
     for treatment in "${treatments[@]}"; do
-        number_of_unique_sessions=$(cat "$out_dir"/session_ids-"$treatment".txt | wc -l)
-        number_of_sl_accesses=$(grep "GET \/credit-card-smart-landing.*" "$out_dir"/localhost_access-$treatment.txt | wc -l)
-        number_of_slideshow_renders=$(grep "GET \/credit-card-smart-landing.*200$" "$out_dir"/localhost_access-$treatment.txt | wc -l)
-        number_of_direct_to_search=$(grep "GET \/credit-card-smart-landing.*302$" "$out_dir"/localhost_access-$treatment.txt | wc -l)
+        number_of_unique_sessions=$(cat "$out_dir"/"$campaign_type"_session_ids-"$treatment".txt | wc -l)
+        number_of_sl_accesses=$(grep "GET \/credit-card-smart-landing.*" "$out_dir"/"$campaign_type"_localhost_access-$treatment.txt | wc -l)
+        number_of_slideshow_renders=$(grep "GET \/credit-card-smart-landing.*200$" "$out_dir"/"$campaign_type"_localhost_access-$treatment.txt | wc -l)
+        number_of_direct_to_search=$(grep "GET \/credit-card-smart-landing.*302$" "$out_dir"/"$campaign_type"_localhost_access-$treatment.txt | wc -l)
         wrong_zero_offers_in_t3=0
 
         if [ $treatment = "t3" ]; then
-            number_of_slideshow_renders=$(grep -Eo "SL_LANDING(,INIT_SESSION2)?,CREATE_LEAD_BEFORE_SEARCH" "$out_dir"/session_activity-t3.txt | wc -l)
-            number_of_direct_to_search=$(grep -Eo "SL_LANDING(,INIT_SESSION2)?(,POST_ELIG)?,SEARCH" "$out_dir"/session_activity-t3.txt | wc -l)
-            wrong_zero_offers_in_t3=$(grep -Eo "SL_LANDING(,INIT_SESSION2)?(,POST_ELIG)?,SEARCH,ZERO_OFFERS" "$out_dir"/session_activity-t3.txt | wc -l)
+            number_of_slideshow_renders=$(grep -Eo "SL_LANDING(,INIT_SESSION2)?,CREATE_LEAD_BEFORE_SEARCH" "$out_dir"/"$campaign_type"_session_activity-t3.txt | wc -l)
+            number_of_direct_to_search=$(grep -Eo "SL_LANDING(,INIT_SESSION2)?(,POST_ELIG)?,SEARCH" "$out_dir"/"$campaign_type"_session_activity-t3.txt | wc -l)
+            wrong_zero_offers_in_t3=$(grep -Eo "SL_LANDING(,INIT_SESSION2)?(,POST_ELIG)?,SEARCH,ZERO_OFFERS" "$out_dir"/"$campaign_type"_session_activity-t3.txt | wc -l)
         fi
         perc_of_renders=$((number_of_slideshow_renders*100/(number_of_slideshow_renders+number_of_direct_to_search)))
 
         printf "%10s %15s %15s %20s %5s %20s %20s\n" $treatment $number_of_unique_sessions $number_of_sl_accesses $number_of_slideshow_renders $perc_of_renders $number_of_direct_to_search $wrong_zero_offers_in_t3
     done
+
+    echo "Number of times SEARCH has been called from SL URL for AndroidWebview = " `grep "AndroidWebview.*GET \/credit-card\/search.html.*smartLanding=true.*newSlideshow=true" "$out_dir"/"$campaign_type"_localhost_access-t3.txt | awk 'BEGIN{FS="\t"}{print $6}' | wc -l`    
 }
 
-function analyze_requests_wo_ajaxcontent() {
-    grep -P "okhttp" "$out_dir"/session_activity-t*.txt \
-        | awk '{print $2}' \
-        | grep -f - "$out_dir"/localhost_access-t*.txt \
-        | grep -v okhttp \
-        | awk 'BEGIN {FS="\t"}{print $4}' \
-        | sed -r 's/.*\(.*Android [.0-9]*; //g; s/\).*//g; s/en-[a-zA-Z]{2}; //g; s/; wv//g' \
-        | sort | uniq \
-        > "$out_dir"/ua_analysis_emitting_devices.txt
-    echo "Devices which requested with UserAgent okhttp has been written to " "$out_dir"/ua_analysis_emitting_devices.txt
-
-    grep "SL_LANDING,$" "$out_dir"/session_activity-*.txt \
-        | grep -v okhttp \
-        | awk 'BEGIN{OFS=FS="\t"}{print $3,$4}' \
-        | sort  | uniq \
-        > "$out_dir"/ua_analysis_proper_wo_ajax_content.html
-    echo "Proper UserAgents (not okhttp) for which we didnt see ajax-content call are exported to " "$out_dir"/ua_analysis_proper_wo_ajax_content.html
-}
-
-function analyze_android_webview_ua {
-    echo "Number of times SEARCH has been called from SL URL for AndroidWebview = " `grep "AndroidWebview.*GET \/credit-card\/search.html.*smartLanding=true.*newSlideshow=true" "$out_dir"/localhost_access-t3.txt | awk 'BEGIN{FS="\t"}{print $6}' | wc -l`    
+function main() {
+    clear
+    initialize
+    for campaign_type in "sms" "email"; do
+        printf "\n%s\n" $(head -c 100 < /dev/zero | tr '\0' '#')
+        center_print "Analysis of $campaign_type Sessions" 
+        printf "%s\n" $(head -c 100 < /dev/zero | tr '\0' '#')
+        parse_session_ids_which_got_CC_SL "$campaign_type"
+        categorize_sessions_based_on_treatments "$campaign_type"
+        compute_session_activity "$campaign_type"
+        analyze_requests_wo_ajaxcontent "$campaign_type"
+        print_summary "$campaign_type"
+    done
 }
 
 start_time=$(($(date +%s%N)/1000000))
-
-#initialize
-#parse_session_ids_which_got_CC_SL
-#categorize_sessions_based_on_treatments
-compute_session_activity
-
-#print_summary
-#analyze_requests_wo_ajaxcontent
-#analyze_android_webview_ua
-
+main
 end_time=$(($(date +%s%N)/1000000))
-echo ""
-echo "Total Time Taken = " $((end_time-start_time)) "ms"
+printf "\nTotal Time Taken = %dms\n" $((end_time-start_time))
